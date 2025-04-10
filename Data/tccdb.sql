d5dbfc36075fbe44d522f43755aac2b526a50459
marianapedroso <marianaleonardo40@outlook.com>
2025-04-10 11:07:17 -0300

    Alterações corrigidas banco de dados

--- Data/tccdb.sql
+++ Data/tccdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 10/04/2025 às 14:06
+-- Tempo de geração: 10/04/2025 às 16:03
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -32,8 +32,17 @@ CREATE TABLE `aluno` (
   `Nome` varchar(255) NOT NULL,
   `RA` varchar(50) NOT NULL,
   `Curso` varchar(255) NOT NULL,
-  `Periodo` varchar(50) NOT NULL
+  `Periodo` varchar(50) NOT NULL,
+  `Senha` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `aluno`
+--
+
+INSERT INTO `aluno` (`Id`, `Nome`, `RA`, `Curso`, `Periodo`, `Senha`) VALUES
+(1, 'Gustavo', '0766231', 'Tads', '4° Semestre', '$2y$10$Xju1jLhWCAVc/s3xqT5uRO0DXpgpCN2vX9mjlkOQ8cooGUstkCLNW'),
+(2, 'Teste Aluno', '123456789', 'Teste', '5° Semestre', '$2y$10$xThXS6srB89ZLi/5XnOAO.pDlAlwicBphi6zfUSJ8vWYtcmylC2Fu');
 
 -- --------------------------------------------------------
 
@@ -99,7 +108,12 @@ CREATE TABLE `coordenador` (
 --
 
 INSERT INTO `coordenador` (`Id`, `Nome`, `RA`, `Curso`, `Senha`) VALUES
-(7, 'teste', '12345678', 'teste', '$2y$10$Eot.4qnaD/3ib5QK3CFmKufRRa.Bdv4.1Aa/SUTvwLv9XKfA/4voO');
+(1, 'Gustavo', '0766231', 'Tads', ''),
+(2, 'Pedro IVO', '099987', 'teste', ''),
+(4, 'teste', '2323', 'teste', '$2y$10$QxyuaYZNQA093sI1LFPDbOpqR9NBMhjAaO0yhvb3MUiPbpUD0IgDy'),
+(5, 'Teste ', '9514960', 'Tads', '$2y$10$uTibiPwfm3W.N/S9pxvX/.tQA/OzGN9xISl7hxphMP89XD5ROD3cG'),
+(6, 'Teste ', '9514961', 'Tads', '$2y$10$yB/OZer7.7nxOZV3NiqxZORF4Bkh3dsFGUwqsa6wyzc9WpNvC4cOm'),
+(7, 'Teste', '123123', 'Teste', '$2y$10$9Tl7aGQDQ0UMxwI6zHIt9OtX9Ctlls./tHdZ7x8KLngIB7vQQ4rjS');
 
 -- --------------------------------------------------------
 
@@ -177,7 +191,7 @@ ALTER TABLE `cursos`
 -- AUTO_INCREMENT de tabela `aluno`
 --
 ALTER TABLE `aluno`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de tabela `atividadecomplementar`
